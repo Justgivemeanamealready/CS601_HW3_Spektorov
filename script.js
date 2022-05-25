806eaf21094844c8e7dedf249393e50e7129d595
David Spektorov <dspektor@bu.edu>
2022-05-25 16:43:57 -0400

    Commitment

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+alert(
+  "Mellow Salutations my fellow parsons, might fine day for filling out a questioneer is it?"
+);
+let input = prompt("Please enter your name", "NAME HERE");
+alert("Greetings " + input);
+let combo = prompt(
+  `Fantastic ${input}, now please give me two intigers separated by a space.`,
+  "X Y"
+);
